ad6963ba08c9e4b2b74e7d9850e5aa1653c36859
xask00 <xask.linus@gmail.com>
2025-05-04 21:36:26 +0530

    bug fix

--- dags/simple_dag.py
+++ dags/simple_dag.py
@@ -3,7 +3,6 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 import time
 
-# Define Python functions
 def print_hello():
     print("Hello from Airflow")
 
@@ -11,7 +10,6 @@ def wait():
     time.sleep(5)
     print("Waited for 5 seconds")
 
-# Default DAG args
 default_args = {
     'owner': 'airflow',
     'start_date': datetime(2023, 1, 1),
@@ -19,7 +17,22 @@ default_args = {
     'retry_delay': timedelta(minutes=1),
 }
 
-# Define DAG
 with DAG(
     dag_id='simple_test_dag',
-    default_args_
+    default_args=default_args,
+    schedule_interval='@daily',
+    catchup=False,
+    tags=['test']
+) as dag:  # ← this line was missing the closing parenthesis earlier
+
+    task1 = PythonOperator(
+        task_id='print_hello',
+        python_callable=print_hello
+    )
+
+    task2 = PythonOperator(
+        task_id='wait_task',
+        python_callable=wait
+    )
+
+    task1 >> task2
