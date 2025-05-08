84143c78fb41c198ed3b6f3ff7ddadff7e097bcf
xask00 <xask.linus@gmail.com>
2025-05-08 10:44:18 +0530

    fix

--- dags/xask_spark_parallel_v2.py
+++ dags/xask_spark_parallel_v2.py
@@ -1,30 +1,29 @@
+import yaml
+import os
 from airflow import DAG
 from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
 from airflow.utils.dates import days_ago
 from airflow.models import Variable
 
 NUM_JOBS = int(Variable.get("NUM_SPARK_JOBS", default_var=100))
+TMP_DIR = "/opt/airflow/dags/tmp_specs"
+os.makedirs(TMP_DIR, exist_ok=True)
 
-default_args = {
-    "start_date": days_ago(1),
-}
+default_args = {"start_date": days_ago(1)}
 
 with DAG(
-    dag_id="xask_spark_parallel",
+    dag_id="xask_spark_parallel_v2",
     default_args=default_args,
     schedule_interval=None,
     catchup=False,
-    tags=["spark", "parallel"],
 ) as dag:
 
     for idx in range(NUM_JOBS):
-        app_spec = {
+        job_name = f"spark-pi-{idx}"
+        spec = {
             "apiVersion": "sparkoperator.k8s.io/v1beta2",
             "kind": "SparkApplication",
-            "metadata": {
-                "name": f"spark-pi-{idx}",
-                "namespace": "spark-apps",
-            },
+            "metadata": {"name": job_name, "namespace": "spark-apps"},
             "spec": {
                 "type": "Scala",
                 "mode": "cluster",
@@ -49,10 +48,14 @@ with DAG(
             },
         }
 
+        file_path = os.path.join(TMP_DIR, f"{job_name}.yaml")
+        with open(file_path, "w") as f:
+            yaml.dump(spec, f)
+
         SparkKubernetesOperator(
-            task_id=f"spark_job_batch_{idx}",
+            task_id=f"spark_job_{idx}",
             namespace="spark-apps",
-            application_file=app_spec,
+            application_file=file_path,
             kubernetes_conn_id="kubernetes_default",
             do_xcom_push=False,
         )
